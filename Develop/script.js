62b160f9b79722428463128026b980bf5e393ca8
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-05-23 20:56:29 -0400

    minor touchup

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,6 @@ var locationLat;
 
 
 function gatherLatLon(city, state) {
-
     fetch(`https://api.geoapify.com/v1/geocode/search?city=${city}&state=${state}&format=json&apiKey=50289ffe2e1e48c89c814826023cb2c0`)
     .then(function (response) {
         return response.json();
@@ -20,19 +19,13 @@ function gatherLatLon(city, state) {
         loggingInfo(locationLat, locationLon);
         
   });
-    /*
-    fetch(`https://api.geoapify.com/v1/geocode/search?city=${city}&state=${state}&format=json&apiKey=50289ffe2e1e48c89c814826023cb2c0`)
-    .then(response => response.json())
-    .then(result => locationLon = result.results[0].lon)
-    .catch(error => console.log('error', error));
-    
-    fetch(`https://api.geoapify.com/v1/geocode/search?city=${city}&state=${state}&format=json&apiKey=50289ffe2e1e48c89c814826023cb2c0`)
-    .then(response => response.json())
-    .then(result => locationLat = result.results[0].lat)
-    .catch(error => console.log('error', error));
-    */
-    
 };
+
+function loggingInfo(lat, long) {
+    console.log(lat);
+    console.log(long);
+
+}
 
 
 
@@ -44,9 +37,3 @@ formButton.addEventListener("click", function() {
 
     //loggingInfo(locationLat, locationLon);
 });
-
-function loggingInfo(lat, long) {
-    console.log(lat);
-    console.log(long);
-
-}
